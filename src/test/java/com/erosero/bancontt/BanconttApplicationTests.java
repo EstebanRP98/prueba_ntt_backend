2ec3f2bd4a1ad200ebf9740f574d5d0a33b59289
EstebanRP98 <davidesteb98@gmail.com>
2022-08-31 21:49:42 -0500

    Inicializando el proyecto

---
+++ src/test/java/com/erosero/bancontt/BanconttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erosero.bancontt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanconttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
